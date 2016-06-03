e73871aa81b82c5108a761924554778312026603
Patrick Jauernig <patrickjauernig@gmail.com>
2016-06-03 17:05:28 +0200

    Support for Android 6 permission management in CellLocationCondition

--- app/src/main/java/org/secuso/privacyfriendlywifi/logic/preconditions/CellLocationCondition.java
+++ app/src/main/java/org/secuso/privacyfriendlywifi/logic/preconditions/CellLocationCondition.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Parcel;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import org.secuso.privacyfriendlywifi.logic.types.PrimitiveCellInfo;
 import org.secuso.privacyfriendlywifi.logic.types.PrimitiveCellInfoTreeSet;
@@ -17,6 +18,7 @@ import java.util.Set;
  *
  */
 public class CellLocationCondition implements Precondition {
+    private static final String TAG = CellLocationCondition.class.getCanonicalName();
     public final int MIN_CELLS = 3;
     public final double MIN_CELL_PERCENTAGE = 0.3;
     private Set<PrimitiveCellInfo> relatedCells;
@@ -32,10 +34,18 @@ public class CellLocationCondition implements Precondition {
 
     @Override
     public boolean check(Context context, Object obj) {
-        Set<PrimitiveCellInfo> currentCells = (Set<PrimitiveCellInfo>) obj;
-        HashSet<PrimitiveCellInfo> union = new HashSet<>(currentCells);
-        union.retainAll(this.relatedCells);
-        return ((double) union.size() / (double) this.relatedCells.size()) > MIN_CELL_PERCENTAGE || union.size() > MIN_CELLS;
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Set<PrimitiveCellInfo> currentCells = (Set<PrimitiveCellInfo>) obj;
+            HashSet<PrimitiveCellInfo> union = new HashSet<>(currentCells);
+            union.retainAll(this.relatedCells);
+
+            // return true if there are enough known cells in neighborhood or if there are at least MIN_CELLS known cells
+            return ((double) union.size() / (double) this.relatedCells.size()) > MIN_CELL_PERCENTAGE || union.size() >= MIN_CELLS;
+        } else {
+            Log.e(TAG, "ACCESS_COARSE_LOCATION not granted.");
+            return false;
+        }
     }
 
     public void addKBestSurroundingCells(Context context, int k, PrimitiveCellInfoTreeSet cells) {
