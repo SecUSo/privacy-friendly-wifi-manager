7d6c95042956d7b157efb4bbb02de2f82a916e77
Patrick Jauernig <patrickjauernig@gmail.com>
2016-06-03 18:26:27 +0200

    Code refactoring and cleanup for preconditions

--- app/src/main/java/org/secuso/privacyfriendlywifi/logic/preconditions/Precondition.java
+++ app/src/main/java/org/secuso/privacyfriendlywifi/logic/preconditions/Precondition.java
@@ -1,17 +1,63 @@
 package org.secuso.privacyfriendlywifi.logic.preconditions;
 
 import android.content.Context;
+import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
  * Interface representing a precondition to check for.
  */
-public interface Precondition extends Parcelable {
+public class Precondition implements Parcelable {
+    protected boolean isActive = true; // by default a user created precondition is active
+
+    public Precondition() {
+    }
+
+    public Precondition(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    protected Precondition(Parcel in) {
+        this.isActive = in.readInt() != 0;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
 
     /**
      * Check whether the precondition applies.
      *
      * @return True, if the precondition applies.
      */
-    boolean check(Context context, Object obj);
+    boolean check(Context context, Object obj) {
+        return this.isActive;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(isActive ? 1 : 0);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Precondition> CREATOR = new Creator<Precondition>() {
+        @Override
+        public Precondition createFromParcel(Parcel in) {
+            return new Precondition(in);
+        }
+
+        @Override
+        public Precondition[] newArray(int size) {
+            return new Precondition[size];
+        }
+    };
 }
--- app/src/main/java/org/secuso/privacyfriendlywifi/logic/preconditions/ScheduleCondition.java
+++ app/src/main/java/org/secuso/privacyfriendlywifi/logic/preconditions/ScheduleCondition.java
@@ -7,48 +7,47 @@ import android.util.Pair;
 /**
  *
  */
-public class ScheduleCondition implements Precondition {
+public class ScheduleCondition extends Precondition {
     private final int start_hour, start_minute, end_hour, end_minute;
-    private boolean isActive;
 
-    public ScheduleCondition(int start_hour, int start_minute, int end_hour, int end_minute, boolean isActive) {
+    public ScheduleCondition(int start_hour, int start_minute, int end_hour, int end_minute) {
+        super();
         this.start_hour = start_hour;
         this.start_minute = start_minute;
         this.end_hour = end_hour;
         this.end_minute = end_minute;
-        this.isActive = isActive;
     }
 
-    public int getStartHour() {
-        return start_hour;
-    }
-
-    public int getStartMinute() {
-        return start_minute;
-    }
-
-    public int getEndHour() {
-        return end_hour;
-    }
-
-    public int getEndMinute() {
-        return end_minute;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean isActive) {
-        this.isActive = isActive;
+    protected ScheduleCondition(Parcel in) {
+        super(in);
+        this.start_hour = in.readInt();
+        this.start_minute = in.readInt();
+        this.end_hour = in.readInt();
+        this.end_minute = in.readInt();
     }
 
     @Override
     public boolean check(Context context, Object obj) {
         Pair<Integer, Integer> hourAndMinute = (Pair<Integer, Integer>) obj;
         // check, whether isActive and the given (current) time lies in between the start and end time
-        return isActive && start_hour <= hourAndMinute.first && hourAndMinute.first <= end_hour
-                && start_minute <= hourAndMinute.second && hourAndMinute.second <= end_minute;
+        return super.check(context, obj) && this.start_hour <= hourAndMinute.first && hourAndMinute.first <= this.end_hour
+                && this.start_minute <= hourAndMinute.second && hourAndMinute.second <= this.end_minute;
+    }
+
+    public int getStartHour() {
+        return this.start_hour;
+    }
+
+    public int getStartMinute() {
+        return this.start_minute;
+    }
+
+    public int getEndHour() {
+        return this.end_hour;
+    }
+
+    public int getEndMinute() {
+        return this.end_minute;
     }
 
     @Override
@@ -58,19 +57,11 @@ public class ScheduleCondition implements Precondition {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
         dest.writeInt(start_hour);
         dest.writeInt(start_minute);
         dest.writeInt(end_hour);
         dest.writeInt(end_minute);
-        dest.writeInt(isActive ? 1 : 0);
-    }
-
-    protected ScheduleCondition(Parcel in) {
-        this.start_hour = in.readInt();
-        this.start_minute = in.readInt();
-        this.end_hour = in.readInt();
-        this.end_minute = in.readInt();
-        this.isActive = in.readInt() != 0;
     }
 
     public static final Creator<ScheduleCondition> CREATOR = new Creator<ScheduleCondition>() {
