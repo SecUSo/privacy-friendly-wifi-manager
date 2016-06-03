883198ae2bb07e38aa8a479a64ddba46bf429ca4
Patrick Jauernig <patrickjauernig@gmail.com>
2016-06-03 18:27:22 +0200

    Hide/show floating action button depending on list scroll

--- app/src/main/java/org/secuso/privacyfriendlywifi/view/fragment/ScheduleFragment.java
+++ app/src/main/java/org/secuso/privacyfriendlywifi/view/fragment/ScheduleFragment.java
@@ -85,6 +85,7 @@ public class ScheduleFragment extends Fragment {
         RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
         recyclerView.addItemDecoration(new DividerItemDecoration(getContext()));
 
+        // TODO example list
         List<ScheduleEntry> scheduleEntries = new ArrayList<>();
         scheduleEntries.add(new ScheduleEntry("Test nummer 1", 1, 2, 3, 4));
         scheduleEntries.add(new ScheduleEntry("Test blabla 1", 2, 3, 4, 15));
