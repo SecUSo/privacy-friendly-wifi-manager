41524f543bdc03c610aef98c57bc443d4f538401
Jan He <jonny0815@gmail.com>
2016-06-21 13:34:28 +0200

    optimized imports

--- app/src/main/java/org/secuso/privacyfriendlywifi/view/WifiWidget.java
+++ app/src/main/java/org/secuso/privacyfriendlywifi/view/WifiWidget.java
@@ -7,7 +7,6 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
 import org.secuso.privacyfriendlywifi.service.Controller;
