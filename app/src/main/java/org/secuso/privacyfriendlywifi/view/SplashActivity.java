b38558134835b0d18dfa3d197ba1a3266f20cd73
Karola Marky <yonjuni@pinyto.de>
2018-03-15 11:01:15 +0100

    SplashScreen added

---
+++ app/src/main/java/org/secuso/privacyfriendlywifi/view/SplashActivity.java
@@ -0,0 +1,40 @@
+/*
+ This file is part of Privacy Friendly Wifi Manager.
+ Privacy Friendly Wifi Manager is free software:
+ you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation,
+ either version 3 of the License, or any later version.
+ Privacy Friendly Wifi Manager is distributed in the hope
+ that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with Privacy Friendly Wifi Manager. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.secuso.privacyfriendlywifi.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.secuso.privacyfriendlywifi.view.tutorial.TutorialActivity;
+
+/**
+ * @author Karola Marky
+ * @version 20161022
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//TODO Tutorial 
+        Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+        SplashActivity.this.startActivity(mainIntent);
+        SplashActivity.this.finish();
+    }
+
+}
