25664965dda60cd14d04deea9dea2ef4a706cf9c
Karola Marky <yonjuni@pinyto.de>
2018-03-15 13:44:00 +0100

    Cleanup of unused drawables, update of license

--- app/src/main/java/org/secuso/privacyfriendlywifi/view/AboutActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlywifi/view/AboutActivity.java
@@ -1,17 +1,18 @@
 /*
- This file is part of Privacy Friendly Wifi Manager.
- Privacy Friendly Wifi Manager is free software:
- you can redistribute it and/or modify it under the terms of the
- GNU General Public License as published by the Free Software Foundation,
- either version 3 of the License, or any later version.
- Privacy Friendly Wifi Manager is distributed in the hope
- that it will be useful, but WITHOUT ANY WARRANTY; without even
- the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- See the GNU General Public License for more details.
- You should have received a copy of the GNU General Public License
- along with Privacy Friendly Wifi Manager. If not, see <http://www.gnu.org/licenses/>.
- */
+Copyright 2016-2018 Jan Henzel, Patrick Jauernig, Dennis Werner
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package org.secuso.privacyfriendlywifi.view;
 
 import android.os.Bundle;
